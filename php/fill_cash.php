e64bf20aa6944d9f6d6a1737301dd68f3f7bdfb0
Yoonmee <ymhwang119@naver.com>
2017-12-13 17:54:09 +0900

    fill_cash 수정

--- php/fill_cash.php
+++ php/fill_cash.php
@@ -6,32 +6,32 @@ $success = true;
 
 
 //트랜잭션 시작
-$result = @mysqli_query($con, "SET AUTOCOMMIT=0");
-$result = @mysqli_query($con, "BEGIN");
+$result = @odbc_do($con, "SET AUTOCOMMIT=0");
+$result = @odbc_do($con, "BEGIN");
 
-
-$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MariaDB: " . mysqli_connect_error();
-}
+//
+// $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+// if (mysqli_connect_errno())
+// {
+//   echo "Failed to connect to MariaDB: " . mysqli_connect_error();
+// }
 
 
 $cash = $_POST['cash'];
 $id = $_POST['user_id'];
-$sql = "UPDATE user SET cash = cash + $cash WHERE id='$id'";
-$result = @mysqli_query($con, $sql);
-if (!$result || @mysqli_affeted_rows($result)==0)
+$sql = "UPDATE NHC_USER SET cash = cash + $cash WHERE id='$id'";
+$result = @odbc_do($con, $sql);
+if (!$result || @odbc_fetch_row($result)==0)
   $success = false;
 
 
 if (!$success) {
-  $result = @mysqli_query("ROLLBACK", $con);
+  $result = @odbc_do("ROLLBACK", $con);
   echo "롤백되었습니다.";
 } else {
-  $result = @mysqli_query("COMMIT", $con);
+  $result = @odbc_do("COMMIT", $con);
   echo "입력되었습니다.";
 }
 
-mysqli_close($con);
+odbc_close($con);
 ?>

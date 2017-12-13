54c2c3ef5d36778fdd63cddfc435a341a13251c9
ChoYunMin <dbsals0618@naver.com>
2017-12-14 02:22:46 +0900

    modify add play

--- php/add_play.php
+++ php/add_play.php
@@ -6,7 +6,7 @@ $play_name = $_POST['play_name'];
 $runningtime = $_POST['runningtime'];
 $price = $_POST['price'];
 
-$screen_time = strtotime($runningtime);
+$screen_time = date("Y-m-d", strtotime($runningtime));
 
 $sql = "INSERT INTO NHC_PLAY(ID, NAME, RUNNINGTIME, PRICE) VALUES ('{$play_id}', '{$play_name}', '{$screen_time}', '{$price}')";
 $result = odbc_exec($con, $sql);
@@ -14,4 +14,6 @@ $result = odbc_exec($con, $sql);
 odbc_close($con);
 ?>
 
+<!--
 <meta http-equiv="refresh" content="0;url=../admin/index.php">
+-->
